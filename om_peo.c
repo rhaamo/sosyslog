279716255e498a7d34a56b6850ad569e96e4202d
claudio <claudio@corest.com>
2000-04-26 21:40:50 +0000

    minor changes on om_peo RMD160End function on rmd160.c

--- om_peo.c
+++ om_peo.c
@@ -39,7 +39,7 @@ static char copyright[] =
 
 #ifndef lint
 /*static char sccsid[] = "@(#)syslogd.c	8.3 (Berkeley) 4/4/94";*/
-static char rcsid[] = "$Id: om_peo.c,v 1.8 2000/04/26 20:14:18 gera Exp $";
+static char rcsid[] = "$Id: om_peo.c,v 1.9 2000/04/26 21:40:50 claudio Exp $";
 #endif /* not lint */
 
 /*
@@ -180,12 +180,12 @@ om_peo_init(argc, argv, f, prog, context)
 				/* set method */
 				if (strcasecmp(optarg, "md5") == 0)
 					hash_method = MD5; 
-				else
-					if (strcasecmp(optarg, "sha1") == 0)
 						hash_method = SHA1;
 				else
 					if (strcasecmp(optarg, "rmd160") == 0)
 						hash_method = RMD160;
+				else
+					if (strcasecmp(optarg, "sha1") == 0)
 				else {
 					if (keyfile != default_keyfile)
 						free(keyfile);
