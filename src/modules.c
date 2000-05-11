7a38fb3e9ea3f95574e53658f3b0cde05fe81ea7
claudio <claudio@corest.com>
2000-05-11 17:47:43 +0000

    little bug

--- src/modules.c
+++ src/modules.c
@@ -1,4 +1,4 @@
-/*	$Id: modules.c,v 1.54 2000/05/11 17:13:33 claudio Exp $
+/*	$Id: modules.c,v 1.55 2000/05/11 17:47:43 claudio Exp $
  * Copyright (c) 1983, 1988, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
  *
@@ -50,7 +50,7 @@
 int om_peo_doLog(struct filed *, int, char *, struct om_header_ctx *);
 int om_peo_init(int, char **, struct filed *, char *, struct om_header_ctx **);
 int om_peo_close(struct filed *, struct om_header_ctx **);
-int om_peo_flush(struct filed *, struct om_headerctx *);
+int om_peo_flush(struct filed *, struct om_header_ctx *);
 
 int om_classic_doLog(struct filed *, int , char *, struct om_header_ctx *);
 int om_classic_init(int, char **, struct filed *, char *, struct om_header_ctx **);
@@ -104,12 +104,12 @@ modules_load()
 #endif
   
 	/* peo module */
-	OModule[OM_PEO].om_name			= "peo";
-	OModule[OM_PEO].om_type			= OM_PEO;
-	OModule[OM_PEO].om_doLog		= om_peo_doLog;
-	OModule[OM_PEO].om_init			= om_peo_init;
-	OModule[OM_PEO].om_close		= om_peo_close;
-	OModule[OM_PEO].om_flush		= om_peo_flush;
+	OModules[OM_PEO].om_name		= "peo";
+	OModules[OM_PEO].om_type		= OM_PEO;
+	OModules[OM_PEO].om_doLog		= om_peo_doLog;
+	OModules[OM_PEO].om_init		= om_peo_init;
+	OModules[OM_PEO].om_close		= om_peo_close;
+	OModules[OM_PEO].om_flush		= om_peo_flush;
 
 	IModules[IM_BSD].im_name		= "bsd";
 	IModules[IM_BSD].im_type		= IM_BSD;
