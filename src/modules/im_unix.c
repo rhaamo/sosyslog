fb2ff97e8fe6d541e0f1465090dd2a7b19936d31
fgsch <fgsch@corest.com>
2000-05-29 20:41:39 +0000

    copyright; remove cvthname declaration

--- src/modules/im_unix.c
+++ src/modules/im_unix.c
@@ -1,4 +1,33 @@
-/*	$CoreSDI: im_unix.c,v 1.19 2000/05/26 21:33:39 fgsch Exp $	*/
+/*	$CoreSDI: im_unix.c,v 1.20 2000/05/27 02:04:00 alejo Exp $	*/
+
+/*
+ * Copyright (c) 2000, Core SDI S.A., Argentina
+ * All rights reserved
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither name of the Core SDI S.A. nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /*
  * im_unix -- classic behaviour module for BSD like systems
@@ -24,12 +53,8 @@
 #include "modules.h"
 #include "syslogd.h"
 
-
-char   *cvthname __P((struct sockaddr_in *));
-
-
 /*
- * get messge
+ * get message
  *
  */
 
