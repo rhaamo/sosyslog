ba44e82dfd8bd24fdc9341f0136a010a1c6cafd6
alejo <alejo@corest.com>
2000-06-01 22:55:58 +0000

    Minor typos on im_mymodule

--- src/modules/im_mymodule.c
+++ src/modules/im_mymodule.c
@@ -1,4 +1,4 @@
-/*	$CoreSDI: im_mymodule.c,v 1.4 2000/05/29 21:12:09 fgsch Exp $	*/
+/*	$CoreSDI: im_mymodule.c,v 1.5 2000/05/29 23:36:18 alejo Exp $	*/
 
 /*
  * Copyright (c) 2000, Core SDI S.A., Argentina
@@ -44,7 +44,7 @@
 #include "syslogd.h"
 
 /* standard input module header variables in context */
-struct im_udp_ctx {
+struct im_mymodule_ctx {
 	short	flags;
 #define M_FLAG_INITIALIZED 0x1
 #define M_FLAG_ERROR 0x2
@@ -58,7 +58,7 @@ struct im_udp_ctx {
  */
 
 int
-im_udp_getLog(im, ret)
+im_mymodule_getLog(im, ret)
 	struct i_module	*im;
 	struct im_msg	*ret;
 {
@@ -66,12 +66,12 @@ im_udp_getLog(im, ret)
 }
 
 /*
- * initialize udp input
+ * initialize mymodule input
  *
  */
 
 int
-im_udp_init(I, argv, argc)
+im_mymodule_init(I, argv, argc)
 	struct i_module *I;
 	char   **argv;
 	int   argc;
@@ -80,7 +80,7 @@ im_udp_init(I, argv, argc)
 }
 
 int
-im_udp_close(im) 
+im_mymodule_close(im) 
         struct i_module *im;
 {
 
