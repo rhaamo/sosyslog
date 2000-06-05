01931fbf1ac840715bdb9adc6d76144a9f104500
claudio <claudio@corest.com>
2000-06-05 23:21:46 +0000

    symbol translation functions; not done

--- src/modules/im_linux.c
+++ src/modules/im_linux.c
@@ -1,4 +1,4 @@
-/*	$CoreSDI: im_linux.c,v 1.6 2000/06/03 01:02:20 claudio Exp $	*/
+/*	$CoreSDI: im_linux.c,v 1.7 2000/06/05 22:59:23 fgsch Exp $	*/
 
 /*
  * Copyright (c) 2000, Core SDI S.A., Argentina
@@ -55,9 +55,36 @@
 #include "syslogd.h"
 
 
+/*
+ * kernel symbols
+ */
+
+#define PATH_KSYM	"/proc/ksyms"
+#define MAX_ADDR_LEN	16
+#define MAX_NAME_LEN	80
+#define MAX_MNAME_LEN	20
+
+typedef struct {
+	char addr[MAX_ADDR_LEN + 1];
+	char name[MAX_NAME_LEN + 1];
+	char mname[MAX_MNAME_LEN + 1];
+} Symbol;
+
+int	 symbols = 0;
+int	 offset = 0;
+FILE	*ksym_fd = NULL;
+Symbol	*symbol_table = NULL;
+
+int	 ksym_init(struct i_module*);
+int	 ksym_close();
+Symbol	*ksym_lookup(Symbol*, char*);
+int	 ksym_getSymbol(Symbol*);
+int	 ksym_parseLine(char*, Symbol*);
+int	 ksym_copy(char*, char*, int);
+
 
 /*
- * initialize linux kernel input
+ * initialize linux kernel input module
  */
 
 int
@@ -73,7 +100,7 @@ im_linux_init(I, argv, argc)
 		I->im_path = NULL;
 		I->im_fd = 0;
 	} else 
-		dprintf("can't open %s (%d)\n", _PATH_KLOG, errno);
+		dprintf("can't open %s: %s\n", _PATH_KLOG, strerror(errno));
 
         I->im_type = IM_LINUX;
         I->im_name = "linux";
@@ -82,10 +109,10 @@ im_linux_init(I, argv, argc)
 }
 
 
-
 /*
- * get kernel messge
- * Take input line from /proc/kmsg or syslog(2), split and format similar to syslog().
+ * get kernel messge:
+ * take input line from _PATH_KLOG or klogctl(2),
+ * split and format similar to syslog().
  */
 
 int
@@ -105,19 +132,15 @@ im_linux_getLog(im, ret)
 		readed = read(im->im_fd, im->im_buf, sizeof(im->im_buf));
 	else
 		/* readed = klogctl(2, im->im_buf, sizeof(im->im_buf)); */ /* this blocks */
-		readed = klogctl(4, im->im_buf, sizeof(im->im_buf));/*this don't block,testing!*/
+		readed = klogctl(4, im->im_buf, sizeof(im->im_buf));	/* this don't block... testing */
 
-	if (readed < 0)
-		if (errno != EINTR) {
-			logerror("im_linux_getLog");
-			return(-1);
-		}
+	if (readed < 0 && errno != EINTR) {
+		logerror("im_linux_getLog");
+		return(-1);
+	}
 
 	im->im_buf[readed-1] = '\0';
 
-	/* parse kernel and module symbols (include priority) */
-	;;;
-	;;;
 	/* get priority */
 	i = 0;
 	if (readed >= 3 && im->im_buf[0] == '<' && im->im_buf[2] == '>' && isdigit(im->im_buf[1])) {
@@ -128,7 +151,11 @@ im_linux_getLog(im, ret)
 	else
 		pri = LOG_WARNING;	/* from printk.c: DEFAULT_MESSAGE_LOGLEVEL */
 
-	/* log it */
+	/* parse kernel/module symbols */
+	;;;
+	;;;
+
+	/* log msg */
 	ret->im_len = snprintf(ret->im_msg, sizeof(ret->im_msg), "kernel: %s", &im->im_buf[i]);
 	strncpy(ret->im_host, LocalHostName, sizeof(ret->im_host));
 	ret->im_host[sizeof(ret->im_host)-1] = '\0';
@@ -198,41 +225,28 @@ im_linux_close(im)
 }
 
 
-
 /*
- * Kernel Symbols Management
+ * open/load symbol table
  */
 
-#define PATH_KSYM	"/proc/ksyms"
-#define MAX_ADDR_LEN	16
-#define MAX_NAME_LEN	80
-#define MAX_MNAME_LEN	20
-
-typedef struct Symbol {
-	char addr[MAX_ADDR_LEN];
-	char name[MAX_NAME_LEN];
-	char mname[MAX_MNAME_LEN];
-};
-
-int symbols = 0;
-FILE *ksym_fd = NULL;
-Symbol *symbol_table = NULL;
-
-
-/* open/init symbol table */
 int
 ksym_init(I)
 	struct i_module *I;
 {
 	ksym_close();
 	if ( (ksym_fd = fopen(PATH_KSYM, "r")) < 0) {
-		/* leer la ksym a mano */
+		/* read ksym using syscalls */
+		;;;
+		;;;
 	}
 	return(0);
 }
 
 
-/* close/delete symbol table */
+/*
+ * close/delete symbol table
+ */
+
 void
 ksym_close()
 {
@@ -248,118 +262,109 @@ ksym_close()
 }
 
 
-/* lookup symbol */
+/*
+ * lookup symbol:
+ * search for a symbol on internal table/file that
+ * matches an address.
+ * if the symbol do not exists it returns NULL
+ */
+
 Symbol*
 ksym_lookup(sym, addr)
 	Symbol *sym;
 	char *addr;
 {
-	while (ksym_getSymbol(sym)) {
-		if (!ksym_getSymbol(sym))
-			break;
+	/* reset symbol table/file */
+	if (ksym_fd < 0)
+		offset = 0;
+	else
+		fseek(ksym_fd, 0, SEEK_SET);
+
+	/* search for symbol */
+	while(ksym_getSymbol(1, sym))
 		if (!strcasecmp(sym->addr, addr))
 			return(sym);
-	}
+
 	return(NULL);
 }
 
 
 /*
  * Get a symbol from file or table
- *
- * int
- * ksym_getSymbol(op, sym)
- *	int op;
- *	Symbol *sym;
- *
- * op = 0 => reset, get first symbol on table
- *    = 1 => get next symbol
- *
- * returns: 0 => no more symbols on table
- *	    1 => symbol loaded on sym
  */
+
 int
-ksym_getSymbol(op, sym)
-	int op;
+ksym_getSymbol(sym)
 	Symbol *sym;
 {
-	static int offset = 0;
 	int readed;
 	char msg[MAXLINE];
 
-	switch(op) {
-		case 0: /* reset, and get first symbol */
-			if (ksym_fd < 0)
-				offset = 0;
-			else
-				fseek(ksym_fd, 0, SEEK_SET);
-
-		case 1: /* get next symbol */
-		default:
-			if (ksym_fd != NULL) {
-				if (++offset == symbols)
-					return(0);
-				sym = symbol_table[offset];
-			} else {
-				if ( (readed = fgets(msg, MAXLINE, ksym_fd)) <= 0)
-					return(0);
-				if (!ksym_parseLine(msg, sym))
-					
-			}
+	if (ksym_fd != NULL) {
+		if (offset == symbols)
+			return(0);
+		sym = symbol_table[offset++];
+	} else {
+		if ( (readed = fgets(msg, MAXLINE, ksym_fd)) <= 0)
+			return(0);
+		return(ksym_parseLine(msg, sym));
 	}
 	return(1);
 }
 
 
 /*
- * ksym_parseLine (readed from ksyms file)
+ * ksym_parseLine (converts a line onto a Symbol)
  * returns 1 on success and 0 on error
  */
+
+#define QUIT_BLANK(p)	while(isblank(*p) && *p != '\0' && *p != '\n') p++;
+
 int
-ksym_parseLine (msg, sym)
-	char *msg;
+ksym_parseLine (p, sym)
+	char *p;
 	Symbol *sym;
 {
-	int addr_offset;
-	int name_offset;
-	int module_offset;
-	char *p;
-
-	if (sym == NULL || msg == NULL || msg[0] == '\0')
+	if (sym == NULL || p == NULL || p[0] == '\0')
 		return(0);
 
-	addr_offset = 0;
-	name_offset = 0;
-	module_offset = 0;
-	p = msg;
-
-	/* search beginning of address */
-	while(isblank(*p) || *p == '\0' || *p == '\n')
-		p++;
+	/* copy symbol address */
+	QUIT_BLANK(p);
 	if (*p == '\0' || *p == '\n')
 		return(0);
-	addr_offset = (int)(p - msg - 1);
+	if (!ksym_copy(sym->addr, p, MAX_ADDR_LEN))
+		return(0);
 
-	/* search beginning of symbol name */
-	while(!isblank(*p) && *p == '\0' && *p == '\n')
-		p++;
+	/* copy symbol name */
+	QUIT_BLANK(p);
 	if (*p == '\0' || *p == '\n')
 		return(0);
-	*(p-1) = '\0';
-	while(isblank(*p) || *p == '\0' || *p == '\n')
-		p++;
-	name_offset = (int)(p - msg - 1);
+	ksym_copy(sym->name, p, MAX_NAME_LEN);
 
-	/* search beginning of module symbol name */
-	while(!isblank(*p) && *p == '\0' && *p == '\n')
-		p++;
+	/* copy module name (if any) */
+	QUIT_BLANK(p);
+	ksym_copy(sym->mname, p, MAX_MNAME_LEN);
 
-
-		HACER BIEN ESTO!
-
-
-	}
-
-	return(0);
+	return(1);
 }
+
+
+/*
+ * copy
+ */
+
+int
+ksym_copy (src, dst, max)
+	char *dst;
+	char *src;
+	int   max;
+{
+	int i = 0;
+
+	while (!isblank(*src) && *src != '\0' && *src != '\n' && i < max)
+		dst[i++] = *src++;
+	dst[i] = '\0';
+	return((*src == '\0' || *src == '\n') ? 0 : 1);
+}
+
 
