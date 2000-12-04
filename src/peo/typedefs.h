8ec3d75ec000443498abf51f1f5dbe9962d00ed4
alejo <alejo@corest.com>
2000-12-04 23:26:53 +0000

    Forgot typedefs.h on peo and to remove static_modules.{c,h}

---
+++ src/peo/typedefs.h
@@ -0,0 +1,29 @@
+#ifndef HAVE_UINT32_T
+# ifdef HAVE_U_INT32_T
+   typedef uint32_t u_int32_t;
+# else
+#  error Could not determine unsigned int 32 typedef
+# endif
+#endif
+
+#ifndef HAVE_UINT64_T
+# ifdef HAVE_U_INT64_T
+   typedef uint64_t u_int64_t;
+# elif defined(HAVE___UINT64_T)
+   typedef __uint64_t uint64_t;
+# else
+#  error Could not determine unsigned int 64 typedef
+# endif
+#endif
+
+/* if __P isn't already defined... */
+#ifdef __STDC__
+# ifndef __P
+#  define       __P(p)  p
+# endif
+#else
+# ifndef __P
+#  define       __P(p)  ()
+# endif
+#endif /* __STDC__ */
+
