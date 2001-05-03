46c9952e68e122656f030ac823360e5b6e317cd1
alejo <alejo@corest.com>
2001-05-03 20:42:22 +0000

    do setlinebuf() once

--- src/syslogd.c
+++ src/syslogd.c
@@ -1,4 +1,4 @@
-/*	$CoreSDI: syslogd.c,v 1.196 2001/04/28 01:28:47 alejo Exp $	*/
+/*	$CoreSDI: syslogd.c,v 1.197 2001/04/28 01:32:53 alejo Exp $	*/
 
 /*
  * Copyright (c) 1983, 1988, 1993, 1994
@@ -41,7 +41,7 @@ static char copyright[] =
 
 #ifndef lint
 /*static char sccsid[] = "@(#)syslogd.c	8.3 (Berkeley) 4/4/94";*/
-static char rcsid[] = "$CoreSDI: syslogd.c,v 1.196 2001/04/28 01:28:47 alejo Exp $";
+static char rcsid[] = "$CoreSDI: syslogd.c,v 1.197 2001/04/28 01:32:53 alejo Exp $";
 #endif /* not lint */
 
 /*
@@ -389,8 +389,7 @@ main(int argc, char **argv)
 			if (fd > 2)
 				close(fd);
 		}
-	} else
-		setlinebuf(stdout);
+	}
 
 	gethostname(LocalHostName, sizeof(LocalHostName));
 	if ((p = strchr(LocalHostName, '.')) != NULL) {
