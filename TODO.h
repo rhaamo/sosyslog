e6883c6a262362a0064ed5396536e2f73c5a53ce
Marc Lagrange <rhaamo@gruik.at>
2010-05-16 20:08:08 +0200

    Another big thing done

--- TODO.h
+++ TODO.h
@@ -6,12 +6,4 @@
 /* Define as the return type of signal handlers (int or void).  */
 #define RETSIGTYPE void
 
-#define HAVE_SOCKLEN_T 1
-#define HAVE_UINT32_T 1
-#define HAVE_UINT64_T 1
-#define HAVE_U_INT32_T 1
-#define HAVE___UINT32_T 1
-#define HAVE_U_INT64_T 1
-#define HAVE___UINT64_T 1
-
 #define HAVE_SYS_WAIT_H 1
